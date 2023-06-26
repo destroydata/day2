a9269fe6b0e4a9ac7cfd0fc19845eed87427279b
tjdanqkr <tjdanqkr@gmail.com>
2023-06-26 12:34:07 +0900

    jdbc template

---
+++ src/main/java/com/naver/user/dao/UserDao.java
@@ -0,0 +1,25 @@
+package com.naver.user.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class UserDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+    public Integer login(String id, String pw){
+        String sql = "select id from users where username = ? and password = ?";
+        return jdbcTemplate.queryForObject(sql, Integer.class, id, pw);
+    }
+    public Integer signup(String id, String pw){
+        String sql = "select id from users where username = ? and password = ?";
+        return jdbcTemplate.update(sql, id, pw);
+    }
+
+
+}
--- src/main/java/com/naver/user/service/UserServiceImpl.java
+++ src/main/java/com/naver/user/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.naver.user.service;
 
+import com.naver.user.dao.UserDao;
 import com.naver.user.domain.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,19 +12,24 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     List<User> users = new ArrayList<>();
 
-    public UserServiceImpl() {
-        users.add(new User("id","123"));
-        users.add(new User("id1","123"));
-    }
-
+//    public UserServiceImpl() {
+//        users.add(new User("id","123"));
+//        users.add(new User("id1","123"));
+//    }
+    @Autowired
+    private UserDao userDao;
     @Override
     public boolean login(String id, String pw) {
-
-        for (int i = 0; i < users.size(); i++) {
-            if (users.get(i).getId().equals(id) && users.get(i).getPw().equals(pw))
-                return true;
+        Integer loginedId = userDao.login(id, pw);
+        if(loginedId==null){
+            return false;
         }
-        return false;
+        return true;
+//        for (int i = 0; i < users.size(); i++) {
+//            if (users.get(i).getId().equals(id) && users.get(i).getPw().equals(pw))
+//                return true;
+//        }
+//        return false;
     }
 
     @Override
